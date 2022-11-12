1f35fddf77d4af63b7324dcc9ea5cc4381b57d11
Isaac Fimbres <fimbresisaac23@gmail.com>
2022-11-12 03:49:53 -0800

    Fixing the Text bug

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
 import HomeScreen from './screens/HomeScreen';
 import CompareLocationsScreen from './screens/CompareLocations';
