1cf4bcfaffb07460ea5b4929733de9ec0bc0a8e0
Alexis <AlexisIVVI@outlook.com>
2022-11-12 04:20:56 -0800

    Avances 2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
@@ -7,7 +8,7 @@ import HomeScreen from './screens/HomeScreen';
 import CompareLocationsScreen from './screens/CompareLocations';
 import SearchLocationScreen from './screens/SearchLocationScreen';
 import SeeLocationScreen from './screens/SeeLocationScreen';
-
+import OptionsLocationScreen from './screens/OptionsLocationScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -20,11 +21,12 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'>
+        <Stack.Navigator initialRouteName='OptionsLocation'>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="CompareLocations" component={CompareLocationsScreen} />
           <Stack.Screen name="SearchLocation" component={SearchLocationScreen} />
           <Stack.Screen name="SeeLocation" component={SeeLocationScreen} />
+          <Stack.Screen name="OptionsLocation" component={OptionsLocationScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
