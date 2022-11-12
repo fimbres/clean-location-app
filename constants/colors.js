bca57d53b518c814d5c1670cc3c5059fe3259e66
Ariel F. Garcia <ariel.incgarcia@gmail.com>
2022-11-12 01:10:06 -0800

    feat/colors: se agrego el objeto para los  colores

---
+++ constants/colors.js
@@ -0,0 +1,16 @@
+const colors = [
+  {
+    StrongGreen: "#395144",
+  },
+  {
+    LightGreen: "#4E6C50",
+  },
+  {
+    StrongBlack: "#2C2727",
+  },
+  {
+    LightBlack: "#3E3939",
+  }
+];
+
+console.log(JSON.stringify(colors));
