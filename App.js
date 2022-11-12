77b5b92e9f16fdef4ca01425ac2e19992fc99cea
Ariel F. Garcia <ariel.incgarcia@gmail.com>
2022-11-12 01:41:25 -0800

    se ha agregado vector icons y custom font

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'VarelaRound-Regular': require('./assets/fonts/VarelaRound-Regular.ttf'),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Ionicons name="home" size={32} color="black" />
+      <Text style={{ fontFamily: 'VarelaRound-Regular', fontSize:16}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
