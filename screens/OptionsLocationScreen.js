87f3c60b02d08e4ee2f245505172eac204814d21
Isaac Fimbres <fimbresisaac23@gmail.com>
2022-11-12 10:13:21 -0800

    minor changes

--- screens/OptionsLocationScreen.js
+++ screens/OptionsLocationScreen.js
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 import OptionCity from '../components/OptionCity';
 import Travel from '../assets/travelers.png';
+
 const OptionsLocationScreen = ({ navigation }) => {
     const [focus, setFocus] = useState(false)
     const [text,onChangeText] = useState("");
     const [options, setOptions] = useState(false);
     const styles_inpt = StyleSheet.create({
         input: {
-            fontSize: 20,
+            fontSize: 16,
             alignContent: "center",
             margin: 30,
             padding: 15,
@@ -20,56 +21,45 @@ const OptionsLocationScreen = ({ navigation }) => {
             
         }
     })
-    const changeInpt = (e) => {
+    const changeInput = (e) => {
         onChangeText(e.valueOf());
         fetch('https://api.geoapify.com/v1/geocode/search?city='+e.valueOf() +'&type=city&format=json&apiKey=32bd4e25f7ee4f78804fcb071ef00171&limit=5')
         .then((res) => res.json())
         .then( (res) => {
             if(res.results){
-                // let opciones = []
-                // res.results.map(direccion => {
-                //     if(opciones){
-                //         let repetido = false
-                //         opciones.map(op => {
-                //             if(op.city == direccion.city && op.country == direccion.country)
-                //                 repetido = true
-                //         })
-                //         if(!repetido)
-                //             opciones.push(direccion)
-                //     }
-                // })
                 setOptions(res.results)
             }
             else 
-                setOptions(false)
+                setOptions([])
         })
         .catch(err => {
-            console.log('HUBO UN ERROR');
+            console.error('HUBO UN ERROR');
         })
     }
   return (
     <View style={styles.container}>
         <Text style={styles.title}>Where do yo want to travel?</Text>
         <TextInput
-        style={styles_inpt.input}
-        onChangeText={changeInpt}
-        selectionColor="#31905D"
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        value={text}
-        ></TextInput>
+            style={styles_inpt.input}
+            onChangeText={changeInput}
+            selectionColor="#31905D"
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            value={text}
+            placeholder='Example: Washington D. C.'
+            placeholderTextColor='#999999'
+        />
         {
-            text == "" || !options ?
-            <Image styles={styles.image} source={Travel}/>
+            text === "" || !options ?
+                <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+                    <Image style={styles.image} source={Travel} />
+                </View>
             :
             options.map((opcion) => {
                 return (
-                    <OptionCity id={opcion.lon + " " + opcion.lat} city={opcion.city} country={opcion.country} navigation={navigation}>
-
-                    </OptionCity>
+                    <OptionCity id={opcion.lon + " " + opcion.lat} city={opcion.city} country={opcion.country} navigation={navigation} />
                 )
             })
-            
         }
     </View>
   )
@@ -84,14 +74,14 @@ const styles = StyleSheet.create({
         color: "#ffffff"
     },
     title: {
-        marginTop: 16,
+        marginTop: 40,
         fontSize: 18,
         textAlign: "center",
         fontWeight: "bold",
         color: "#ffffff"
     },
     image: {
-        width: 100,
-        height: 100,
+        width: 330,
+        height: 330,
     }
 });
