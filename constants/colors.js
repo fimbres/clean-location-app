aaa8ee65fe2b9ba637f3ac412139eb5ff101d2d7
Ariel F. Garcia <ariel.incgarcia@gmail.com>
2022-11-12 02:00:06 -0800

    add colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,14 +1,13 @@
 export const colors = [
   {
     StrongGreen: "#395144",
-  },
-  {
     LightGreen: "#4E6C50",
-  },
-  {
     StrongBlack: "#2C2727",
-  },
-  {
     LightBlack: "#3E3939",
+    YellowStar: "#FFD233",
+    White: "#F6F4F4",
+    GreenGradient: "#04FF1D",
+    YellowGradient: "#FF9900",
+    RedGradient: "#FF0000",
   }
 ];
