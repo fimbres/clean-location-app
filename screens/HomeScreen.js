2df18bf98fb556a91a414063a67337f2337b09fe
Isaac Fimbres <fimbresisaac23@gmail.com>
2022-11-12 03:58:37 -0800

    updating HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Marker } from 'react-native-maps';
+
+import Map from '../components/Map';
+import DangerDot from '../assets/danger-dot.png';
+import SecureDot from '../assets/secure-dot.png';
+import WarningDot from '../assets/warning-dot.png';
 
 const HomeScreen = ({ navigation }) => {
+    const sampleSiteMarkers = [
+        {
+            coordinate: {
+                longitude: -106.391015,
+                latitude: 39.085855
+            }
+        },
+        {
+            coordinate: {
+                longitude: -106.368051,
+                latitude: 39.351661
+            }
+        },
+        {
+            coordinate: {
+                longitude: -106.389204,
+                latitude: 39.372171
+            }
+        }
+    ];
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Map>
+        {sampleSiteMarkers.map((marker, index) => {
+            return (
+                <Marker 
+                    key={index}
+                    coordinate={marker.coordinate}
+                    image={DangerDot}
+                />
+            );
+        })}
+      </Map>
     </View>
   )
 }
